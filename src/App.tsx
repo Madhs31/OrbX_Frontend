f978d8b028248ac344f1fb0b0eb3af7f3a4d27eb
Madhs31 <fernandinha.mf30@gmail.com>
2025-11-13 08:09:59 -0300

    ♻️ refactor: Melhorando estrutura principal da aplicação em App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,35 @@ import Home from './pages/Home';
 import ContinentPage from './pages/ContinentPage';
 import CountryPage from './pages/CountryPage';
 import CityPage from './pages/CityPage';
-import LoginPage from './pages/LoginPage'; // Você já está importando o componente aqui
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import DashboardPage from './pages/DashboardPage';
+// Importe a nova página de Admin aqui:
+import AdminPanel from './pages/AdminPage'; 
 import './styles/global.css';
 
 function App() {
-return (
-<Router>
-<div className="App">
-<Navbar />
-<div className="container">
-<Routes>
-<Route path="/" element={<Home />} />
-<Route path="/continents" element={<ContinentPage />} />
-<Route path="/countries" element={<CountryPage />} />
-<Route path="/cities" element={<CityPage />} />
-      {/* Esta rota agora usa o LoginPage importado do arquivo './pages/LoginPage' */}
-<Route path="/login" element={<LoginPage />} /> 
-</Routes>
-</div>
-</div>
-</Router>
-);
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/continents" element={<ContinentPage />} />
+            <Route path="/countries" element={<CountryPage />} />
+            <Route path="/cities" element={<CityPage />} />
+            <Route path="/login" element={<LoginPage />} /> 
+            <Route path="/register" element={<RegisterPage />} /> 
+            <Route path="/dashboard" element={<DashboardPage />} />
+            
+            {/* NOVA ROTA ADICIONADA: */}
+            <Route path="/admin" element={<AdminPanel />} /> 
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
